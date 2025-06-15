5cbc5bef18b66b23f55aa4b594a1690e1cae3f6f
Jaisilan Nadar <jaysheelannadar2003@gmail.com>
2025-06-15 13:53:20 +0530

    /dist for deploy test

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = "http://localhost:5000/api/v1";
-export const BASE_URL = "http://192.168.43.71:5000/api/v1";
+// export const BASE_URL = "http://192.168.43.71:5000/api/v1";
+export const BASE_URL = "https://wavetrack.onrender.com/api/v1";
