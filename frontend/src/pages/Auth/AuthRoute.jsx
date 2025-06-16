799144759aefe2a58fb54148cd03bda36fffed1e
Jaisilan Nadar <jaysheelannadar2003@gmail.com>
2025-06-16 17:46:16 +0530

    bug fix dv1

--- frontend/src/pages/Auth/AuthRoute.jsx
+++ frontend/src/pages/Auth/AuthRoute.jsx
@@ -20,7 +20,7 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 const AuthRoute = ({ children }) => {
-  const { isAuthenticated } = useSelector((state) => state.auth);
+  const { isAuthenticated } = useSelector((state) => state?.auth);
   return isAuthenticated ? children : <Navigate to="/login" replace />;
 };
 
