bb1b171e0ccfa3ff234ced16cdcfb6907b9b6752
Jaisilan Nadar <jaysheelannadar2003@gmail.com>
2025-08-08 13:03:16 +0530

    SuperLink deploy url fix

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -2,4 +2,4 @@
 // export const BASE_URL = "http://192.168.0.109:5000/api/v1";
 // export const BASE_URL = "https://wavetrack.onrender.com/api/v1";
 // export const BASE_URL = "https://wavetrack-service.onrender.com/api/v1";
-export const BASE_URL = "https://superlink-wavetrack.onrender.com";
+export const BASE_URL = "https://superlink-wavetrack.onrender.com/api/v1";
