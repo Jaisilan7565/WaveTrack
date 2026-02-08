97982dad1c754703a7d784bd1c5ae36a120e346c
Jaisilan Nadar <jaisilan.n@unniailabs.com>
2026-02-08 23:34:55 +0530

    url update

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -2,4 +2,5 @@
 // export const BASE_URL = "http://192.168.0.109:5000/api/v1";
 // export const BASE_URL = "https://wavetrack.onrender.com/api/v1";
 // export const BASE_URL = "https://wavetrack-service.onrender.com/api/v1";
-export const BASE_URL = "https://superlink-wavetrack.onrender.com/api/v1";
+// export const BASE_URL = "https://superlink-wavetrack.onrender.com/api/v1";
+export const BASE_URL = import.meta.env.VITE_API_URL;
