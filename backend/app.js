c846642416f11fca1a4ca0fda80212b76b22b899
Jaisilan Nadar <jaisilan.n@unniailabs.com>
2026-04-18 14:49:34 +0530

    multiple frontend URL can be added now

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === "development") {
 
 // Enable CORS
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  origin: [process.env.FRONTEND_URL || "http://localhost:3000"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   optionsSuccessStatus: 200,
